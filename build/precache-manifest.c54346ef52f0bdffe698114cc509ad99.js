b458fd829cb3b1bd81ea4ec5bead44e362d5c314
lucas cavalcante <48697826+lucaxdev@users.noreply.github.com>
2020-05-24 13:47:54 -0400

    Add files via upload

---
+++ build/precache-manifest.c54346ef52f0bdffe698114cc509ad99.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b0d52da86d6b39e4dc1efa714d8395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79f778ad96f1aa0b2e7a",
+    "url": "/static/css/2.f3011563.chunk.css"
+  },
+  {
+    "revision": "cbfe81c0e413641f7fa6",
+    "url": "/static/css/main.b530ae40.chunk.css"
+  },
+  {
+    "revision": "79f778ad96f1aa0b2e7a",
+    "url": "/static/js/2.6eaa6a83.chunk.js"
+  },
+  {
+    "revision": "91b059d36dc0e248974136ffdcb31335",
+    "url": "/static/js/2.6eaa6a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbfe81c0e413641f7fa6",
+    "url": "/static/js/main.0dd91f74.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "f530397417aa1ac5dc44a0451feec47d",
+    "url": "/static/media/heroes.f5303974.png"
+  },
+  {
+    "revision": "9d12efb0afd8be45ae58c4658f4b9b95",
+    "url": "/static/media/logo.9d12efb0.svg"
+  }
+]);
